af586fe8f099b865c99c3dd37dd499b86fae510d
Brickiss <amandabricki85@gmail.com>
2025-05-08 18:30:43 -0300

    Coletor

---
+++ ColetaSeletiva/trajetocoletaseletivajlle/src/main/java/br/futurodev/joinville/trajetocoletaseletivajlle/models/Coletor.java
@@ -0,0 +1,39 @@
+package br.futurodev.joinville.trajetocoletaseletivajlle.models;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Coletor {
+        @Getter
+        private static final List<Coletor> collectors = new ArrayList<>();
+        private static Long sequence = 1L;
+
+        @Setter(AccessLevel.NONE)
+        private Long id;
+        private String name;
+        private String description;
+
+        public Coletor() {
+            this.id = sequence++;
+        }
+
+        public static Coletor addColetor(Coletor collector) {
+            collectors.add(collector);
+            return collector;
+        }
+
+        @Override
+        public String toString() {
+            return "Coletor{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", description='" + description + '\'' +
+                    '}';
+        }
+    }
