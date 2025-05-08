043f47aa8e20191d9a66975ae35e0e43eb7877a8
Brickiss <amandabricki85@gmail.com>
2025-05-08 11:37:59 -0300

    ProjSpring

---
+++ ColetaSeletiva/trajetocoletaseletivajlle/src/main/java/br/futurodev/joinville/trajetocoletaseletivajlle/TrajetocoletaseletivaJlleApplication.java
@@ -0,0 +1,13 @@
+package br.futurodev.joinville.trajetocoletaseletivajlle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrajetocoletaseletivaJlleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrajetocoletaseletivaJlleApplication.class, args);
+	}
+
+}
---
+++ ColetaSeletiva/trajetocoletaseletivajlle/src/test/java/br/futurodev/joinville/trajetocoletaseletivajlle/TrajetocoletaseletivaJlleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.futurodev.joinville.trajetocoletaseletivajlle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrajetocoletaseletivaJlleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
